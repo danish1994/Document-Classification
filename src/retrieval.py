b2570f54aabddc3c1b8e9c783b13875094bd3ae3
Danish Kamal <danish8802204230@gmail.com>
2016-10-23 01:19:47 +0530

    Classification Criteria Added

--- src/retrieval.py
+++ src/retrieval.py
@@ -1,8 +1,8 @@
 import nltk
 import nltk.data
-import itertools  
-from nltk.chunk.named_entity import NEChunkParserTagger,NEChunkParser
+import itertools
 import nltk.tag as tagger
+from nltk.chunk.named_entity import NEChunkParserTagger, NEChunkParser
 from nltk.corpus import PlaintextCorpusReader
 from nltk.corpus import conll2000
 from nltk.corpus import stopwords
@@ -24,6 +24,62 @@ class UnigramChunker(nltk.ChunkParserI):
         conlltags = [(word, pos, chunktag)
                      for ((word, pos), chunktag) in zip(sentence, chunktags)]
         return nltk.chunk.util.conlltags2tree(conlltags)
+
+
+class ConsecutiveNPChunkTagger(nltk.TaggerI):
+
+    def __init__(self, train_sents):
+        train_set = []
+        for tagged_sent in train_sents:
+            untagged_sent = nltk.tag.untag(tagged_sent)
+            history = []
+            for i, (word, tag) in enumerate(tagged_sent):
+                featureset = npchunk_features(untagged_sent, i, history)
+                train_set.append((featureset, tag))
+                history.append(tag)
+        self.classifier = nltk.MaxentClassifier.train(
+            train_set, algorithm='IIS', trace=0)
+        self.classifier.show_most_informative_features(20)
+
+    def tag(self, sentence):
+        history = []
+        for i, word in enumerate(sentence):
+            featureset = npchunk_features(sentence, i, history)
+            tag = self.classifier.classify(featureset)
+            history.append(tag)
+        return zip(sentence, history)
+
+
+class ConsecutiveNPChunker(nltk.ChunkParserI):
+
+    def __init__(self, train_sents):
+        tagged_sents = [[((w, t), c) for (w, t, c) in
+                         nltk.chunk.tree2conlltags(sent)]
+                        for sent in train_sents]
+        self.tagger = ConsecutiveNPChunkTagger(tagged_sents)
+
+    def parse(self, sentence):
+        tagged_sents = self.tagger.tag(sentence)
+        conlltags = [(w, t, c) for ((w, t), c) in tagged_sents]
+        return nltk.chunk.conlltags2tree(conlltags)
+
+
+class TagChunker(nltk.chunk.ChunkParserI):
+
+    def __init__(self, chunk_tagger):
+        self._chunk_tagger = chunk_tagger
+
+    def parse(self, tokens):
+        # split words and part of speech tags
+        (words, tags) = zip(*tokens)
+        # get IOB chunk tags
+        chunks = self._chunk_tagger.tag(tags)
+        # join words with chunk tags
+        wtc = itertools.izip(words, chunks)
+        # w = word, t = part-of-speech tag, c = chunk tag
+        lines = [' '.join([w, t, c]) for (w, (t, c)) in wtc if c]
+        # create tree from conll formatted chunk lines
+        return nltk.chunk.conllstr2tree('\n'.join(lines))
 
 
 def npchunk_features(sentence, i, history):
@@ -64,107 +120,78 @@ def npchunk_features(sentence, i, history):
 #     return '+'.join(sorted(tags))
 
 
-class ConsecutiveNPChunkTagger(nltk.TaggerI):
+def initialize(path):
+    f = open(path)
+    sentence = f.read()
+    stemmer = SnowballStemmer('english')
+    stop = set(stopwords.words('english'))
+    sentence = [i for i in sentence.split() if i not in stop]
+    stemmed = [stemmer.stem(sente) for sente in sentence]
+    sentence = ' '.join(str(e) for e in sentence)
+    stemmed = ' '.join(str(e) for e in stemmed)
 
-    def __init__(self, train_sents):
-        train_set = []
-        for tagged_sent in train_sents:
-            untagged_sent = nltk.tag.untag(tagged_sent)
-            history = []
-            for i, (word, tag) in enumerate(tagged_sent):
-                featureset = npchunk_features(untagged_sent, i, history)
-                train_set.append((featureset, tag))
-                history.append(tag)
-        self.classifier = nltk.MaxentClassifier.train(
-            train_set, algorithm='IIS', trace=0)
-        self.classifier.show_most_informative_features(20)
+    res = zip(sentence,stemmed)
 
-    def tag(self, sentence):
-        history = []
-        for i, word in enumerate(sentence):
-            featureset = npchunk_features(sentence, i, history)
-            tag = self.classifier.classify(featureset)
-            history.append(tag)
-        return zip(sentence, history)
+    return res
 
 
-class ConsecutiveNPChunker(nltk.ChunkParserI):
+def first_criteria(sentence):
+    total_count = 0
 
-    def __init__(self, train_sents):
-        tagged_sents = [[((w, t), c) for (w, t, c) in
-                         nltk.chunk.tree2conlltags(sent)]
-                        for sent in train_sents]
-        self.tagger = ConsecutiveNPChunkTagger(tagged_sents)
+    fdist = FreqDist()
+    for seeent in nltk.tokenize.sent_tokenize(sentence):
+        for word in nltk.tokenize.word_tokenize(seeent):
+            fdist[word] += 1
+            total_count += 1
 
-    def parse(self, sentence):
-        tagged_sents = self.tagger.tag(sentence)
-        conlltags = [(w, t, c) for ((w, t), c) in tagged_sents]
-        return nltk.chunk.conlltags2tree(conlltags)
+    count = fdist['i'] + fdist['I'] + fdist['we'] + \
+        fdist['We'] + fdist['you'] + fdist['You']
 
-class TagChunker(nltk.chunk.ChunkParserI):
-    def __init__(self, chunk_tagger):
-        self._chunk_tagger = chunk_tagger
- 
-    def parse(self, tokens):
-        # split words and part of speech tags
-        (words, tags) = zip(*tokens)
-        # get IOB chunk tags
-        chunks = self._chunk_tagger.tag(tags)
-        # join words with chunk tags
-        wtc = itertools.izip(words, chunks)
-        # w = word, t = part-of-speech tag, c = chunk tag
-        lines = [' '.join([w, t, c]) for (w, (t, c)) in wtc if c]
-        # create tree from conll formatted chunk lines
-        return nltk.chunk.conllstr2tree('\n'.join(lines))
+    return int((count / total_count) * 100)
 
-train_sents = conll2000.chunked_sents('train.txt')
-test_sents = conll2000.chunked_sents('test.txt')
-print(train_sents)
 
-stemmer = SnowballStemmer("english")
-# f = open("DataSet/Fiction/Romantic/Pride-and-Prejudice.txt")
-f = open("vocab.txt")
-sentence = f.read()
-# stop = set(stopwords.words('english'))
-# sentence = [i for i in sentence.split() if i not in stop]
-# plurals = [stemmer.stem(sente) for sente in sentence]
-# # print(sentence)
-# # print(plurals)
-# sentence = ' '.join(str(e) for e in sentence)
-# plurals = ' '.join(str(e) for e in plurals)
-# print(plurals)
-seent = nltk.sent_tokenize(sentence)
-text = nltk.Text(seent)
-tagged = [nltk.word_tokenize(se) for se in seent]
-after_tag = [nltk.pos_tag(ta) for ta in tagged]
-# chunker2 = TagChunker(NEChunkParserTagger);
-# wsj = nltk.corpus.treebank.tagged_words(tagset='universal')
+def second_criteria(sentence):
+    total_count = 0
+
+    fdist = FreqDist()
+    for seeent in nltk.tokenize.sent_tokenize(sentence):
+        for word in nltk.tokenize.word_tokenize(seeent):
+            fdist[word] += 1
+            total_count += 1
+
+    count = fdist["'"] + fdist[':'] + fdist[','] + fdist['-'] + fdist['...'] + \
+        fdist['!'] + fdist['.'] + fdist['?'] + fdist['"'] + fdist[';']
+
+    return int((count / total_count) * 100)
+
+
+def third_criteria(sentence):
+    seent = nltk.sent_tokenize(sentence)
+    text = nltk.Text(seent)
+    tagged = [nltk.word_tokenize(se) for se in seent]
+    after_tag = [nltk.pos_tag(ta) for ta in tagged]
+    # nltk.ne_chunk(after_tag[0]).draw()
+    word_tag_fd = FreqDist(after_tag[0])
+    # print([wt[0] for (wt, _) in word_tag_fd.most_common() if wt[1] == 'NN'])
+
+# train_sents = conll2000.chunked_sents('train.txt')
+# test_sents = conll2000.chunked_sents('test.txt')
+# print(train_sents)
 
 # chunker = ConsecutiveNPChunker(train_sents)
 # print(chunker.evaluate(test_sents))
 
-# Testing for 1st Sentence
-senti = after_tag[0]
-# nltk.ne_chunk(senti).draw()
+sentence,stemmed = zip(*initialize("DataSet/Fiction/Drama/The-Diaries-of-Bunty-Danvers.txt"))
+sentence = ''.join(sentence)
+stemmed = ''.join(stemmed)
 
+# Criteria 1
+criteria_1 = first_criteria(stemmed)
+print(criteria_1)
 
-fdist = FreqDist()
-for seeent in nltk.tokenize.sent_tokenize(sentence):
-    for word in nltk.tokenize.word_tokenize(seeent):
-        fdist[word] += 1
-print(fdist) 
+# Criteria 2
+criteria_2 = second_criteria(stemmed)
+print(criteria_2)
 
-print(type(fdist))
-
-print([wt for (wt, _) in fdist.most_common() if (wt == 'I' || wt == 'we' || wt == 'you')])
-
-
-# word_tag_fd = FreqDist(after_tag[0])
-# print([wt[0] for (wt, _) in word_tag_fd.most_common() if wt[1] == 'NNP'])
-
-# print(text.dispersion_plot(['I', 'we', 'you']))
-
-# for tag in after_tag:
-#     tree = chunker.tagger.tag(tag)
-#     sentence, history = zip(*tree)
-#     chunker.parse(tree)
+# Criteria 3
+third_criteria(sentence)
